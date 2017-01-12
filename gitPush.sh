2eececa65783445611fc435cdeef63e5e653fb50
Sue Jarman <sue@suejarman.co.uk>
2017-01-12 21:52:35 +0000

    Auto commit

--- gitPush.sh
+++ gitPush.sh
@@ -1,3 +1,3 @@
 git add --all
 git commit -am 'Auto commit' --all
-git push https://arch-bc@github.com/arc-bc/arch-bc.github.io.git
+git push https://arch-bc@github.com/arch-bc/arch-bc.github.io.git
